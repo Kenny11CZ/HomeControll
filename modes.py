cc8a2af4049106493cc655896b98b31fa4ae4cac
Server <kenny@gamemovies.eu>
2015-12-11 23:54:29 +0100

    statistics 16 DEBUG

--- modes.py
+++ modes.py
@@ -2,6 +2,8 @@ __author__ = 'Kenny'
 import datetime
 import threading
 
+import pprint
+
 
 #Modes
 def statistics(thermometers, time):
@@ -9,6 +11,7 @@ def statistics(thermometers, time):
         with open('output.txt', 'a+') as f:
             f.write("#####\n"+str(datetime.datetime.now())+"\n")
             for x in thermometers:
+                pprint.pprint(x)
                 f.write("{0}({1}):{2}".format(x.description, str(x.file_id), str(x.GetTemp())))
         threading.Timer(time, LogTemperatures, [thermometers]).start()
 
