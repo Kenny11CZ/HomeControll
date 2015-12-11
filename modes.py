230062ea3a8b26b5c3cd37d059c2ad431488e24b
Server <kenny@gamemovies.eu>
2015-12-11 23:59:38 +0100

    statistics 18 FINAL

--- modes.py
+++ modes.py
@@ -10,7 +10,7 @@ def statistics(thermometers, time):
         with open('output.txt', 'a+') as f:
             f.write("#####\n"+str(datetime.datetime.now())+"\n")
             for x in thermometers:
-                f.write("{0}({1}):{2}".format(x.description, str(x.file_id), str(x.GetTemp())))
+                f.write("{0}({1}):{2}\n".format(x.description, str(x.file_id), str(x.GetTemp())))
         threading.Timer(time, LogTemperatures, [thermometers]).start()
 
 
