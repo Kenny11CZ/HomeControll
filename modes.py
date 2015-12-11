2b6ac0ef785bc348163482b530d50047d9587572
Server <kenny@gamemovies.eu>
2015-12-11 23:41:53 +0100

    statistics 14

--- modes.py
+++ modes.py
@@ -5,15 +5,15 @@ import threading
 
 #Modes
 def statistics(thermometers, time):
-    def LogTemperatures():
+    def LogTemperatures(thermometers):
         with open('output.txt', 'a+') as f:
             f.write("#####\n"+str(datetime.datetime.now())+"\n")
             for x in thermometers:
                 f.write("{0}({1}):{2}".format(x.description, str(x.file_id), str(x.GetTemp())))
-        threading.Timer(time, LogTemperatures).start()
+        threading.Timer(time, LogTemperatures, thermometers).start()
 
 
-    threading.Timer(time, LogTemperatures).start()
+    threading.Timer(time, LogTemperatures, thermometers).start()
     print("Start measurement")
 
 
