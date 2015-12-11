2ef2808f3d694a4979252e82f8936c6b2df459f2
kenny11cz <dan.balarin@gmail.com>
2015-12-11 22:04:18 +0100

    arg parser

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import glob
+import argparse
 sys.path.insert(0, 'Objects')
 from thermometer import Thermometer
 from db import DB
@@ -20,7 +21,15 @@ def FirstRun():
 def Initialize():
     thermometers = Thermometer.LoadThermometers()
 
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('integers', metavar='N', type=int, nargs='+',
+                   help='an integer for the accumulator')
+parser.add_argument('--sum', dest='accumulate', action='store_const',
+                   const=sum, default=max,
+                   help='sum the integers (default: find the max)')
 
+args = parser.parse_args()
+print args.accumulate(args.integers)
 
 if not os.path.isfile("HomeControll.db"):
     FirstRun()
