7eff4442f28665216a1dc67053689fd5e157a214
Server <kenny@gamemovies.eu>
2015-12-12 20:29:28 +0100

    json

--- modes.py
+++ modes.py
@@ -14,9 +14,9 @@ def statistics(thermometers, time):
                 f.write("{0}({1}):{2}\n".format(x.description, str(x.file_id), str(x.GetTemp())))
 
         with open('output2.txt', 'a+') as f:
-            f.write("{{\"time\":{0},\"thermometers\":[".format(str(datetime.datetime.now(),)))
+            f.write("{{\"time\":\"{0}\",\"thermometers\":[".format(str(datetime.datetime.now(),)))
             for x in thermometers:
-                f.write("{{\"id:\"\"{0}\",\"name:{1}\",\"temperature\":\"{2}\"}},".format(x.file_id, x.description, x.GetTemp()))
+                f.write("{{\"id\":\"{0}\",\"name\":\"{1}\",\"temperature\":\"{2}\"}},".format(x.file_id, x.description, x.GetTemp()))
             f.write("]}}")
         threading.Timer(time, LogTemperatures, [thermometers]).start()
         iteration[0] = iteration[0] + 1
