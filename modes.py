8484d131657f944115ffc2586a137284b1a18c4f
Server <kenny@gamemovies.eu>
2015-12-12 22:42:57 +0100

    json

--- modes.py
+++ modes.py
@@ -8,26 +8,29 @@ import json
 def statistics(thermometers, time):
     iteration = [None]
     iteration[0] = 0
-    def LogTemperatures(thermometers, f):
+    def LogTemperatures(thermometers):
         #with open('output.txt', 'a+') as f:
         #    f.write("#####\n"+str(datetime.datetime.now())+"\n")
         #    for x in thermometers:
         #        f.write("{0}({1}):{2}\n".format(x.description, str(x.file_id), str(x.GetTemp())))
+        with open('output.txt', 'r+') as f:
+            data = json.load(f)
 
 
-        data = [None]
-        data[iteration[0]] = {"time":str(datetime.datetime.now())}
+        temp[0] = {"time":str(datetime.datetime.now())}
         therms = list()
         for i, t in enumerate(thermometers):
             therms.append({"id": t.file_id, "name": t.description, "temp": t.GetTemp()})
-        data[iteration[0]]["thermometers"] = therms
+        temp[0]["thermometers"] = therms
+        data.append(temp)
         print(json.dumps(data))
-        threading.Timer(time, LogTemperatures, [thermometers, f]).start()
+        with open('output.txt', 'w+') as f:
+            f.write(data)
+        threading.Timer(time, LogTemperatures, [thermometers]).start()
         iteration[0] = iteration[0] + 1
         print("{0} iteration".format(iteration[0],))
     print("Start measurement")
-    with open('output2.txt', 'ra+') as f:
-        threading.Timer(time, LogTemperatures, [thermometers, f]).start()
+    threading.Timer(time, LogTemperatures, [thermometers]).start()
 
 def pastebin():
     import requests, urllib
